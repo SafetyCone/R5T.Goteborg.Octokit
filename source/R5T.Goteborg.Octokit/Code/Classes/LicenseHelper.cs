072b20ef898ef000f6e73c0008d335e96b4d0165
MinexAutomation <minex.automation@gmail.com>
2019-12-14 04:26:21 -0800

    Implemented CreateRepository() and added LicenseHelper class.

---
+++ source/R5T.Goteborg.Octokit/Code/Classes/LicenseHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.Goteborg.Octokit
+{
+    public class LicenseHelper
+    {
+        public const string MitLicenseIdentifier = "mit";
+    }
+}
--- source/R5T.Goteborg.Octokit/Code/Services/Implementations/GitHubOperator.cs
+++ source/R5T.Goteborg.Octokit/Code/Services/Implementations/GitHubOperator.cs
@@ -1,15 +1,53 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 using Octokit;
+
+using R5T.Magyar;
 
 
 namespace R5T.Goteborg.Octokit
 {
     public class GitHubOperator : IGitHubOperator
     {
-        public void CreateRepository(GitHubRepository repository)
+        private IGitHubClientProvider GitHubClientProvider { get; }
+
+
+        public GitHubOperator(IGitHubClientProvider gitHubClientProvider)
         {
-            throw new NotImplementedException();
+            this.GitHubClientProvider = gitHubClientProvider;
+        }
+
+        public async Task<long> CreateRepository(GitHubRepository repository)
+        {
+            var gitHubClient = this.GitHubClientProvider.GetGitHubClient();
+
+            var autoInit = repository.InitializeWithReadMe;
+
+            string licenseTemplate;
+            switch(repository.License)
+            {
+                case GitHubRepositoryLicense.MIT:
+                    licenseTemplate = LicenseHelper.MitLicenseIdentifier;
+                    break;
+
+                default:
+                    throw new Exception(EnumHelper.UnexpectedEnumerationValueMessage(repository.License));
+            }
+
+            var @private = repository.Visibility == GitHubRepositoryVisibility.Private ? true : false; // Private if private, otherwise public.
+
+            var newRepository = new NewRepository(repository.Name)
+            {
+                AutoInit = true, // Create with README for immediate checkout.
+                Description = repository.Description,
+                LicenseTemplate = licenseTemplate,
+                Private = @private, // Default is public (private = false).
+            };
+
+            var createdRepository = await gitHubClient.Repository.Create(newRepository);
+
+            return createdRepository.Id;
         }
     }
 }
