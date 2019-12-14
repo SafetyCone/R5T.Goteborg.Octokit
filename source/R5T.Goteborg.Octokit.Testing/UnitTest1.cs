b7d05569d4d71a1a360b9f9bd37c37092de0091a
MinexAutomation <minex.automation@gmail.com>
2019-12-14 02:15:10 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Goteborg.Octokit.Testing/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace R5T.Goteborg.Octokit.Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ source/R5T.Goteborg.Octokit/Code/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace R5T.Goteborg.Octokit
+{
+    public class Class1
+    {
+    }
+}
